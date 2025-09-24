e7c90b796cc847cebe7c4dff8132ae2f9b2b7bbb
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-24 19:51:12 -0300

    Add Product entity with attributes and relationships

---
+++ src/main/java/com/armazena/armazena/entities/Product/Product.java
@@ -0,0 +1,51 @@
+package com.armazena.armazena.entities.Product;
+
+import com.armazena.armazena.DTOs.ProductDTO.ProductRequestDTO;
+import com.armazena.armazena.entities.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int quantity;
+
+    private double price;
+
+    private String category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Product(ProductRequestDTO requestDTO, User user) {
+        this.name = requestDTO.name();
+        this.price = requestDTO.price();
+        this.quantity = requestDTO.quantity();
+        this.category = requestDTO.category();
+        this.user = user;
+    }
+}
