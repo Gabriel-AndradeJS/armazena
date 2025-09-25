98eebea8c5453237fe8d75ce84887fdf2df40100
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-25 19:46:02 -0300

    Add product list to UserResponseDTO

--- src/main/java/com/armazena/armazena/DTOs/UserDTO/UserResponseDTO.java
+++ src/main/java/com/armazena/armazena/DTOs/UserDTO/UserResponseDTO.java
@@ -1,11 +1,15 @@
 package com.armazena.armazena.DTOs.UserDTO;
 
+import com.armazena.armazena.entities.Product.Product;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record UserResponseDTO(
         Long id,
         String name,
         String email,
+        List<Product> product,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
