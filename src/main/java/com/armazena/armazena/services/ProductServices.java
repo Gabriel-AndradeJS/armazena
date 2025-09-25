c816fe59741a15e91f5bfa6a1e3500a58c20d2dc
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-25 10:49:50 -0300

    Remove unused import for Optional in ProductServices

--- src/main/java/com/armazena/armazena/services/ProductServices.java
+++ src/main/java/com/armazena/armazena/services/ProductServices.java
@@ -9,7 +9,6 @@ import com.armazena.armazena.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class ProductServices {
