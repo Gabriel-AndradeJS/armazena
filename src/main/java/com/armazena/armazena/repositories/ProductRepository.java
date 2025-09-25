857d7648f76a56880048f9426d7eba21b8f09972
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-09-25 19:45:42 -0300

    Add method to find products by user ID in ProductRepository

--- src/main/java/com/armazena/armazena/repositories/ProductRepository.java
+++ src/main/java/com/armazena/armazena/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.armazena.armazena.repositories;
 import com.armazena.armazena.entities.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByUserId(Long userId);
 }
